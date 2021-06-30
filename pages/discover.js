3f2a4b30a5b2d8572b46125f7ca9601769ad5921
Wei Chun <68662576+weichunnn@users.noreply.github.com>
2021-07-01 01:31:43 +0800

    feature/bookmarks (#5)
    * added side nav
    
    * switched to swr
    
    * added favourites page

--- pages/discover.js
+++ pages/discover.js
@@ -1,4 +1,4 @@
-import { Text, Wrap } from '@chakra-ui/react';
+import { Text, Wrap, Center, Spinner } from '@chakra-ui/react';
 
 import App from '@/components/App';
 import BusinessCard from '@/components/BusinessCard';
@@ -6,50 +6,64 @@ import EmptyState from '@/components/EmptyState';
 import { getAllBusinesses } from '@/lib/db-admin';
 import searchLogic from 'utils/searchLogic';
 import { useSearch } from '@/lib/search';
+import useSWR from 'swr';
+import fetcher from '@/utils/fetcher';
 
 export async function getStaticProps(context) {
   const { businesses } = await getAllBusinesses();
 
   return {
     props: {
-      businesses: businesses
+      initialBusinesses: businesses
     },
     revalidate: 3600
   };
 }
 
-export default function Discover({ businesses }) {
+export default function Discover({ initialBusinesses }) {
   const { location } = useSearch();
-  // General Search Logic
-  const filteredBusinesses = searchLogic(businesses);
+  const { data } = useSWR('/api/businesses', fetcher, {
+    initialBusinesses
+  });
+
+  const businesses = data?.businesses;
+  const filteredBusinesses = searchLogic(
+    typeof data?.businesses == 'undefined' ? [] : data.businesses
+  );
 
   return (
     <App>
       <>
-        {filteredBusinesses?.length ? (
-          <>
-            <Text mb="2" fontSize="sm" fontWeight="bold">
-              Local businesses around you
-            </Text>
-            <Wrap justify="center" spacing="8">
-              {businesses.map((business) => (
-                <BusinessCard key={business.id} {...business} />
-              ))}
-            </Wrap>
-            <Text mt="4" fontSize="sm" align="right">
-              Showing <b>{filteredBusinesses.length}</b> out of{' '}
-              <b>
-                {
-                  businesses.filter((business) =>
-                    location.includes(business.location)
-                  ).length
-                }
-              </b>{' '}
-              businesses in <b>{location}</b>.
-            </Text>
-          </>
+        {businesses?.length ? (
+          filteredBusinesses.length ? (
+            <>
+              <Text mb="4" fontSize="sm" fontWeight="bold">
+                Local businesses around you
+              </Text>
+              <Wrap justify="center" spacing="8">
+                {filteredBusinesses.map((business) => (
+                  <BusinessCard key={business.id} {...business} />
+                ))}
+              </Wrap>
+              <Text mt="4" fontSize="sm" align="right">
+                Showing <b>{filteredBusinesses.length}</b> out of{' '}
+                <b>
+                  {
+                    businesses.filter((business) =>
+                      location.includes(business.location)
+                    ).length
+                  }
+                </b>{' '}
+                businesses in <b>{location}</b>.
+              </Text>
+            </>
+          ) : (
+            <EmptyState />
+          )
         ) : (
-          <EmptyState />
+          <Center mt="24" w="full">
+            <Spinner size="xl" speed="0.5s" />
+          </Center>
         )}
       </>
     </App>
