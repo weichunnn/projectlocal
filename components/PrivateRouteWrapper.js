f7ab27d91941675fbbcfff07b92120e86493663b
Wei Chun <tanweichun@outlook.com>
2021-07-03 22:01:34 +0800

    added private route wrapper

---
+++ components/PrivateRouteWrapper.js
@@ -0,0 +1,30 @@
+import { useAuth } from '@/lib/auth';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Spinner, Center } from '@chakra-ui/react';
+
+const PrivateRouteWrapper = (Component) => (props) => {
+  const { user } = useAuth();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      setLoading(false);
+    } else if (!user & (user != null)) {
+      router.push('/signup');
+    }
+  }, [user]);
+
+  if (loading) {
+    return (
+      <Center h="100vh" bg="gray.100">
+        <Spinner />
+      </Center>
+    );
+  } else {
+    return <Component {...props} />;
+  }
+};
+
+export default PrivateRouteWrapper;
